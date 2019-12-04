36cc08102463afbc41be58d557d7c9a5bda56ed2
Joshua Hoskins <jhoskins@jlab.org>
2019-12-04 14:45:20 -0500

    More fully implemented DataClasses into VarMonitor class. Monitor configuration data now read in as a pandas DataFrame object, parsed, and pushed into the monitor class attribute.
    ## Things added
    - DataClasses functionality
    - Pandas file parsing
    
    ## Things that are needed
    - EPICS variables to Monitor
    - Error checking
    - Analysis functionality

--- ptmonitor.py
+++ ptmonitor.py
@@ -1,10 +1,11 @@
 from ptsmtplib import Chaldea
 from ptvarmonitor import VarMonitor
+from ptvarmonitor import PyStruct as struct
 
 if __name__ == '__main__':
     mail = Chaldea('jrh4dy@virginia.edu')
     monitor = VarMonitor(config_dir='config')
-    monitor.read_config_files('config')
+    data = monitor.read_config_files('config')
 
     #message = "Hey there, python test."
     #mail.send_message(message)
--- ptvarmonitor.py
+++ ptvarmonitor.py
@@ -8,18 +8,27 @@ from dataclasses import dataclass
 class VarMonitor(object):
     def __init__(self, *args, **kawgs):
         self.config = 'config'
+        self.monitor = []
 
         if "config_dir" in kawgs:
             self.config = kawgs["config_dir"]
         else:
-            self.config = "config"
-    def read_config_files(self, config):
+            pass
+    def read_config_files(self, config:str)->pd.DataFrame:
+        """Reads in all configuration files from defined directory. Files are read into a custom "c-like" data
+        structure, PyStruct and appended to monitor class attribute. Function returns a PANDAS DataFrame object."""
+
         for file in glob.glob('{config}/*.config'.format(config=config)):
-            data = pd.read_csv('{file}'.format(file=file), delimiter=',')
-            print(data)
+            data = pd.read_csv('{file}'.format(file=file), delimiter=',').replace(' ', np.nan)
+            self.monitor.append(PyStruct(data['variable_name'][0],
+                           data['epics_name'][0],
+                           data['upper_bound'][0],
+                           data['lower_bound'][0]))
+        return data
+
 @dataclass
-class PyStruct(object):
-        name: str
-        epics_name: str
-        upper_bound: float
-        lower_bound: float
+class PyStruct:
+        name: str = ''
+        epics_name: str = ''
+        upper_bound: float = 0.0
+        lower_bound: float = 0.0
