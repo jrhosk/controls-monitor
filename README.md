bc27087c419d22d1e44d97b57d137c1b94c0cfeb
Josh Hoskins <jhoskins@jlab.org>
2019-12-08 22:16:17 -0500

    Messaging credentials now stored in configuration files handled by the configparse module. Conifiguration file should be stored with the proper group and access right to keep credentials secret.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# controls-monitor
+### Python based EPICS variable alram handling, monitoring, and analysis framework.
+
+Provides
+- Email messaging and reporting.
+- Time series analysis of target system slow controls variables.
+- Potential failure analysis and detection.
--- ptsmtplib.py
+++ ptsmtplib.py
@@ -1,19 +1,22 @@
 import smtplib
 import ssl
 import base64
+import configparser 
 
 class Chaldea(object):
 
     def __init__(self, *args, **kawgs):
-        self.smtp_server = 'smtp.gmail.com'
-        self.port = 465     # For SSL
-        self.sender = 'e1039spinquest@gmail.com'
+        self.config = configparser.ConfigParser()
+        self.config.read('configuration.ini')
+        self.smtp_server = self.config['connection']['server']
+        self.port = self.config['connection']['port']
+        self.user = self.config['credentials']['user']
+        self.password = self.config['credentials']['password']
         self.recipient = {_ for _ in args}
 
-
-    def send_message(self, message):
+    def send_message(self, message:str):
         """Connects to smtp server to send notification, alarms, and analysis reports."""
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL(self.smtp_server, self.port, context=context) as self.server:
-            self.server.login(self.sender, '') #Enter password here
-            self.server.sendmail(self.sender, self.recipient, message)
+            self.server.login(self.user, password)
+            self.server.sendmail(self.user, self.recipient, message)
